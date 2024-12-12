0be859f65a5d2d47d3a5753e58f7f0538df067c0
Nunu <bumbala8888@gmail.com>
2024-12-12 19:19:34 +0100

    solution

---
+++ Task43.py
@@ -0,0 +1,14 @@
+platby = [3000, 200, 18, 34444]
+#ke každý platbě vypíšeme fakturu
+ICO = 3455432
+#Vystavuji fakturu ID 0 od firmy s IČO 3455432 na částku 3000
+#Vystavuji fakturu ID 1 od firmy s IČO 3455432 na částku 200
+#Vystavuji fakturu ID 2 od firmy s IČO 3455432 na částku 18
+#Vystavuji fakturu ID 3 od firmy s IČO 3455432 na částku 34444
+#pomocí for cyklu a enumerate()
+
+for index, platba in enumerate(platby):
+    print(f"Vystavuji fakturu ID {index} od firmy s IČO {ICO} na částku {platba} ")
+
+#Kdybych chtěl číslovat od 1
+#    print(f"Vystavuji fakturu ID {(index)+1} od firmy s IČO {ICO} na částku {platba} ")
