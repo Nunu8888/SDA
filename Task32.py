a863cc9062b1fcd69946335149aadf4ff4974adc
Nunu <bumbala8888@gmail.com>
2024-12-12 15:21:17 +0100

    komplet

---
+++ Task32.py
@@ -0,0 +1,24 @@
+'''
+Zadání:
+Napiš program, který požádá uživatele o zadání čísla odpovídající známce (1 až 5). Program vypíše hodnocení:
+
+1 = "Výborně"
+2 = "Chvalitebně"
+3 = "Dobře"
+4 = "Dostatečně"
+5 = "Nedostatečně"
+Pokud uživatel zadá jiné číslo než 1–5, vypiš: "Neplatná známka".
+'''
+znamka = int(input("Zadej známku"))
+if znamka==1:
+    print("Výborně")
+elif znamka==2:
+    print("Chvalitebně")
+elif znamka==3:
+    print("Dobře")
+elif znamka==4:
+    print("Dostatečně")
+elif znamka==5:
+    print("Nedostatečně")
+else:
+    print("Neplatná známka")
---
+++ Task35_moje.py
@@ -0,0 +1,8 @@
+#pomocí while cyklu dejte do listu čísla od 1 do 20
+#list vyprintujte
+
+list = []
+
+while len(list) < 20:
+    list.append(len(list) + 1)
+print(list)
